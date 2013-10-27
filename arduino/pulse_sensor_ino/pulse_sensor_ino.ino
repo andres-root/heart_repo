86bb1fd4bf5d8711202bf5387cbbcee6f181ae7d
tracke <strike1609@gmail.com>
2013-10-27 06:01:29 -0500

    Calculating beat rate aprox and formating data from Bluethoot to mobile device

--- arduino/pulse_sensor_ino/pulse_sensor_ino.ino
+++ arduino/pulse_sensor_ino/pulse_sensor_ino.ino
@@ -15,21 +15,18 @@ void loop() {
   while(true) {
     if(analogRead(A0)) {
       ret = (int) analogRead(A0);
-      Serial.println(ret);
-      
-      
-      
-     //buff.concat(ret);
-      //buff.concat(",");
+      //Serial.println(ret);                  
+      buff.concat(ret);
+      buff.concat(",");
       //Serial.println(c);
-      /*if(c == 250) {
+      if(c == 25) {
         c = 0;
         Serial.println(buff);
+        ret = 0;
         //Serial.println(c);
       } else {
         c++;
-      }
-      */
+      }      
       //if(ret < 1023) {
         //ret = ret * 10;        
         //Serial.println(analogRead(A0));      
